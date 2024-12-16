3fc9aa6fcfc9b109f8e92f098be2996299050cee
muteeb28 <muteebmasoodi28@gmail.com>
2024-12-16 11:02:28 +0530

    added some examples

---
+++ intro_javascript/variables.js
@@ -0,0 +1,22 @@
+// ### **Ways to Create Variables**
+
+// 1. **`var`**  
+//     Example:
+//    `var marks = 90;`
+   
+// 2. **`let`**  
+//     Example:
+//     `let score = 50;`
+    
+// 3. **`const`**  
+//     Example:
+//     `const x = 200;`
+
+/**
+ * Ways to create variables
+ * var let const 
+ */
+
+var firstName = "Simond";
+let secondName = "Richard";
+const age = 33
