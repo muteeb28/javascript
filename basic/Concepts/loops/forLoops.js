3a48083f5c28e19dcf2a5f9d0b4b0c05652d4641
muteeb28 <muteebmasoodi28@gmail.com>
2025-01-28 16:03:40 +0530

    added for loop

---
+++ basic/Concepts/loops/forLoops.js
@@ -0,0 +1,15 @@
+//recap
+let i =1;
+while(i<=10){
+    console.log(i)
+    i+=1;
+}
+//now lets convert it to a for loop
+for(let i=1; i<=10; i+=1){
+    console.log(i)
+}
+/*
+so for loops are one more way to do same thing as of 
+while loop there is no particular advantage of using a particular loop
+in for loops the difference with while loops is only of synatx
+*/
